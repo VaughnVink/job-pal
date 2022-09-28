143324f2b7751ccce605d5062a67cace0c6e216a
vaughnvink <rivervink@live.nl>
2022-09-29 00:03:38 +1300

    Chore: Vuetify UI colours set-up

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,13 @@ import Vuetify from 'vuetify/lib/framework'
 Vue.use(Vuetify)
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: '#5360c8',
+        secondary: '#480096',
+        accent: '#3f51b5'
+      }
+    }
+  }
 })
